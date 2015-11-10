361fde87b61c1261cf13ee77869f0902b76c9fc6
dave95 <davidegallitelli@gmail.com>
2015-11-10 11:19:00 +0100

    Reorganized folders, uploaded theory, uploaded original PDFs

---
+++ Practica 3/inClase.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int n, *ptr;
+
+int func(void),suma;
+int* func2(void);
+void func3(int (*func)());	//puntatore a dove comincia la funzione
+
+func2 = suma;
+func2();
+
+int main(){
+	
+}
