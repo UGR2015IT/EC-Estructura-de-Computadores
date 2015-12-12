acfab29920aa5281cdea089322bc937eb085b1bc
dave95 <davidegallitelli@gmail.com>
2015-12-12 12:33:59 +0100

    Initial files for Practica 4 EC

---
+++ Practica 4/bomba.c
@@ -0,0 +1,50 @@
+#include <stdio.h>	// para printf()
+#include <stdlib.h>	// para exit()
+#include <string.h>	// para strncmp()/strlen()
+#include <sys/time.h>	// para gettimeofday(), struct timeval
+
+char password[]="abracadabra\n";
+int  passcode  = 7777;
+
+void boom(){
+	printf("***************\n");
+	printf("*** BOOM!!! ***\n");
+	printf("***************\n");
+	exit(-1);
+}
+
+void defused(){
+	printf("*************************\n");
+	printf("*** bomba desactivada ***\n");
+	printf("*************************\n");
+	exit(0);
+}
+
+int main(){
+#define SIZE 100
+	char pass[SIZE];
+	int  pasv;
+#define TLIM 5
+	struct timeval tv1,tv2;	// gettimeofday() secs-usecs
+
+	gettimeofday(&tv1,NULL);
+
+	printf("Introduce la contraseña: ");
+	fgets(pass,SIZE,stdin);
+	if (strncmp(pass,password,strlen(password)))
+	    boom();
+
+	gettimeofday(&tv2,NULL);
+	if (tv2.tv_sec - tv1.tv_sec > TLIM)
+	    boom();
+
+	printf("Introduce el código: ");
+	scanf("%i",&pasv);
+	if (pasv!=passcode) boom();
+
+	gettimeofday(&tv1,NULL);
+	if (tv1.tv_sec - tv2.tv_sec > TLIM)
+	    boom();
+
+	defused();
+}
